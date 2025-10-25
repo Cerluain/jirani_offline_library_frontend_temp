9d404f555c9c670bb20854399127c91fce7e071a
Dhalo25 <leonardo@masinialeman.com>
2025-10-25 14:44:40 -0700

    Pages Implemented

---
+++ jirani-frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route} from 'react-router'
+
+
+import MainDashboard from './pages/MainDashboard'
+import Analytics from './pages/Analytics'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import Signup from './pages/Signup'
+import UploadAndManagement from './pages/UploadAndManagement'
+
+
+function App() {
+
+  return (
+    <>
+    <h1>HELLO WORLD</h1>
+      <Routes>
+          <Route path="/" element={<MainDashboard />} />
+          <Route path="/maindashboard" element={<MainDashboard />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/uploadandmanagement" element={<UploadAndManagement />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ jirani-frontend/src/pages/MainDashboard.jsx
@@ -0,0 +1,5 @@
+export default function MainDashboard(){
+    return(<>
+        MainDashboard
+    </>);
+}
